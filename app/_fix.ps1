fbf64409b5d420d1a188bfba1d78098a55181871
Sut <xepinchan@qq.com>
2026-09-22 18:42:35 +0800

    chore(app): 移除临时诊断脚本与日志

--- app/_fix.ps1
+++
@@ -1,6 +0,0 @@
-# Force flutter tool rebuild: delete stale flutter_tools.stamp
-$stamp = 'E:\Program Files\Flutter\bin\cache\flutter_tools.stamp'
-if (Test-Path $stamp) {
-  Remove-Item $stamp -Force
-}
-"stamp_exists=$(Test-Path $stamp)"
